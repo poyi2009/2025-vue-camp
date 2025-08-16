df0fc2aa17c4b97bc8e4500e1930cc7ae6de392d
poyi <poyi10925@gmail.com>
2025-08-17 03:11:45 +0800

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/2025hex-vue-camp/',
+  base: '/2025-vue-camp/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
